a57561ca6e3c4cfcf11188ce65df7f0422228c3f
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2025-10-17 01:33:28 +0800

    Implement Footer Section

--- src/components/three/Footer.jsx
+++ src/components/three/Footer.jsx
@@ -1,6 +1,23 @@
+import { footerLinks } from "../../constants"
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <footer>
+    <div className="info">
+      <p>More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966</p>
+      <hr />
+      <div className="links">
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+        <ul>
+          {footerLinks.map(({label, link}) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+   </footer>
   )
 }
 
