d2084d8424e920d75057ad0a169ac56140c01693
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2025-10-22 23:52:25 +0800

    Finalize Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,21 +2,30 @@ import { useEffect, useRef } from "react"
 
 const Hero = () => {
 
-const videoRef = useRef();
+const videoRef = useRef(null);
 
 useEffect(() => {
     if (videoRef.current) {
-        videoRef.current.playbackRate = 2;
+        videoRef.current.playbackRate = 1;
+        videoRef.current.play();
     }
  }, []);
 
   return (
-   <section id="hero">
+   <section id="hero" >
         <div>
             <h1>MacBook Pro</h1>
             <img src="/title.png" alt="MacBook Title" />
         </div>
-        <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
+        <div className="relative" >
+        <video 
+            ref={videoRef}  
+            src="/videos/hero.mp4" 
+            autoPlay 
+            muted 
+            playsInline 
+        />
+        </div>
         <button type="button">Buy</button>
         <p>From $1599 0r $133/mo for 12 months</p>
    </section>
